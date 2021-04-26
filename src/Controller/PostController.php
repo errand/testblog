4075bdd22d4d51a8ec21bda888516f1e52ad36dd
Aleksandr Shatskikh <work@errand.ru>
2021-04-26 23:16:14 +0300

    added views for the posts and comments

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class PostController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Environment $twig, PostRepository $postRepository): Response
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        return new Response($twig->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+            ]));
+    }
+
+    /**
+     * @Route("/post/{id}", name="post")
+     */
+    public function show(Environment $twig, Post $post, CommentRepository $commentRepository): Response
+    {
+        return new Response($twig->render('post/show.html.twig', [
+            'post' => $post,
+            'comments' => $commentRepository->findBy(['post' => $post], ['createdAt' => 'DESC']),
+        ]));
     }
 }
