15c0ade617adab28b1f474781574234f5a9088b6
Aleksandr Shatskikh <work@errand.ru>
2021-05-06 18:17:02 +0300

    fixed todo with several words in similarity

--- src/Twig/SimilarityExtention.php
+++ src/Twig/SimilarityExtention.php
@@ -13,20 +13,22 @@ class SimilarityExtention extends AbstractExtension
 		);
 	}
 
-	public function similarityFilter($text, $word)
+	public function similarityFilter($text, $input)
 	{
 		$terms = explode(' ', $text);
+        $words = explode(' ', $input);
 
 		foreach($terms as $i => $term) {
-			similar_text($word, $term, $percent);
-			if($percent >= 75 && $percent <= 85) {
-				$terms[$i] = '<span class="bg-orange text-white">'.$term.'</span>';
-			} else if($percent > 85 && $percent <= 95) {
-				$terms[$i] = '<span class="bg-yellow">'.$term.'</span>';
-			} else if ($percent > 95) {
-				$terms[$i] = '<span class="bg-success text-white">'.$term.'</span>';
-			}
-
+            foreach($words as $word){
+                similar_text($word, $term, $percent);
+                if($percent >= 75 && $percent <= 85) {
+                    $terms[$i] = '<span class="bg-orange text-white">'.$term.'</span>';
+                } else if($percent > 85 && $percent <= 95) {
+                    $terms[$i] = '<span class="bg-yellow">'.$term.'</span>';
+                } else if ($percent > 95) {
+                    $terms[$i] = '<span class="bg-success text-white">'.$term.'</span>';
+                }
+            }
 		}
 		$str = implode(' ', $terms);
 
